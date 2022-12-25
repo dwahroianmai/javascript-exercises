e1ac61c341bc86f35a42e8105bc3b871392a7b6d
dwahroianmai <105296071+dwahroianmai@users.noreply.github.com>
2022-12-25 23:50:55 +0100

    Fix leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 100 === 0) {
+        return year % 400 === 0;
+    } return year % 4 === 0;
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+/* is divisible by 4,
+if divisible by 100 then also by 400 */
