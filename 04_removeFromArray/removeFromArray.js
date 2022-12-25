9f2501515ec21cb4d8ca8a765441281ec840443b
dwahroianmai <105296071+dwahroianmai@users.noreply.github.com>
2022-12-25 18:04:35 +0100

    Fix removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    for (let i = 0; i < args.length; i++) {
+        array = array.filter(arg => arg !== args[i]);
+    } return array;
 };
 
 // Do not edit below this line
