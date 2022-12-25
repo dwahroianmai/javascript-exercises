ce23628d812a8ff745ff73d326ae2e822e3f29da
dwahroianmai <105296071+dwahroianmai@users.noreply.github.com>
2022-12-25 19:15:54 +0100

    Fix sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(numOne, numTwo) {
+    if (numOne < 0 || numTwo < 0 || 
+        typeof numOne != "number" || 
+        typeof numTwo != "number") {
+            return "ERROR";
+        } else {
+            let total = 0;
+            for (let i = Math.min(...arguments); i <= Math.max(...arguments); i++) {
+                total += i;
+            } return total;
+        }
 };
 
 // Do not edit below this line
