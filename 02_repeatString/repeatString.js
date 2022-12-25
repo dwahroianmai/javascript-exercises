0b68e78a61aef4f6c920854e5fce719558dd1651
dwahroianmai <105296071+dwahroianmai@users.noreply.github.com>
2022-12-25 16:50:46 +0100

    Fix repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, n) {
+    let stringCopy = string.slice();
+    if (n === 0) {
+        return '';
+    } else if (n > 0) {
+        for (let i = 0; i < n - 1; i++) {
+            string += stringCopy;
+        } return string;
+    } else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
